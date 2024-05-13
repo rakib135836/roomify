172e032f7509e48d7461ae9e0a50602dd0cc1c3d
rakib <rakib135836@gmail.com>
2024-05-13 11:35:35 +0600

    booking functionality completed

--- src/pages/Rooms/RoomDetails.jsx
+++ src/pages/Rooms/RoomDetails.jsx
@@ -1,8 +1,57 @@
+import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { FirebaseContext } from "../../FirebaseProvider/FirebaseProvider";
+
+
+// date picker necessary import 
+import  { useState } from "react";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+import axios from "axios";
+
+
+// react toast 
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const RoomDetails = () => {
+
+    const [startDate, setStartDate] = useState(new Date());
+    const notify = () => toast("room booked successfully");
+
+
+    const {user}=useContext(FirebaseContext);
     const details = useLoaderData();
-    const { room_type, image, description, price_per_night, room_size, availability, special_offers } = details;
+    const { room_type, image, description, price_per_night, room_size, availability, special_offers,_id } = details;
+
+    const handleSubmit=async e=>{
+        e.preventDefault();
+        // const form=e.target; 
+        const id=_id;
+        const email=user?.email;
+        const userName=user?.displayName;
+        const date=startDate;
+        const type=room_type;
+        const price=price_per_night;
+        const review="";
+
+        const bookingDetails={id,email,userName,date,type,price,review}
+        console.table(bookingDetails);
+
+        try{
+            const {data}= await axios.post(`${import.meta.env.VITE_API_URL}/booking`,bookingDetails)
+            console.log(data)
+            if(data.insertedId){
+                notify();
+            }
+        }
+        catch (err){
+            console.error(err)
+        }
+    }
 
     return (
         <section className="bg-white dark:bg-gray-900">
@@ -33,13 +82,58 @@ const RoomDetails = () => {
 
                             <h3 className="mt-6 text-lg font-medium text-blue-500">Room Size:{room_size}</h3>
                             <p className="text-gray-600 dark:text-gray-300"> special offers  : {special_offers?.length > 0 ? special_offers.join(', ') : "there are no special offers for this room"}</p>
-                            <button className="btn btn-block bg-blue-400 mt-5">Book Now</button>
+
+
+                            {/* modal button  */}
+
+                            {/* Open the modal using document.getElementById('ID').showModal() method */}
+                            <button className="btn btn-block bg-blue-400 mt-5" onClick={() => document.getElementById('my_modal_5').showModal()}>Book Now</button>
+                            <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+
+
+                                <div className="modal-box">
+                                    <h3 className="font-bold text-lg">Price Per Night: $ {price_per_night}</h3>
+                                    <p className="py-4">{description}</p>
+
+                                    {/* form */}
+
+                                    <form onSubmit={handleSubmit}>
+
+                                        <label className="input input-bordered flex items-center gap-2">
+                                            <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+                                            
+                                        </label>
+                                        <label className="input input-bordered flex items-center gap-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
+                                            <input type="email" name="email" defaultValue={user ? user.email : ""} className=" pointer-events-none input input-bordered w-full " readOnly />
+                                        </label>
+                                        <label className="input input-bordered flex items-center gap-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
+                                            <input type="text" name="userName" defaultValue={user ? user.displayName : ""} className="pointer-events-none input input-bordered w-full " readOnly />
+                                        </label>
+                                        
+                                        <input type="submit" className="btn btn-block bg-blue-400" name="" id="" value='confirm order' />
+
+                                    </form>
+
+                                    {/* form */}
+
+
+                                    <div className="modal-action">
+                                        <form method="dialog">
+                                            {/* if there is a button in form, it will close the modal */}
+                                            <button className="btn">Cancel Order</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </dialog>
                         </div>
                     </div>
 
 
                 </div>
             </div>
+            <ToastContainer />
         </section>
     );
 };
